05816f433492e7e169bdf09587aa170e258fa67f
Rushikesh Bagade <rbagade911@gmail.com>
2025-02-10 13:45:55 +0530

    deleted the register and quiz question component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from './components/Homepage'
 import Login from './components/Login'
-import Register from './components/Register'
 import QuizLanding from './components/QuizLanding'
 import Protected from './components/Protected'
 import { useEffect } from 'react'
@@ -30,7 +29,6 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
           <Route path='/quiz-homepage' element={<Protected><QuizLanding /></Protected>} />
           <Route path='/quiz' element={<Protected><QuizPage /></Protected>} />
           <Route path='/leaderboard' element={<Leaderboard />} />
